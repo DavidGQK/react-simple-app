be28079708df9b6a2916394402731021a7b30d52
David <davka8989@gmail.com>
2023-11-06 14:47:01 +0300

    func for adding a new person

--- src/App.js
+++ src/App.js
@@ -37,6 +37,39 @@ function App(props) {
         setCrowd(people);
     }, []);
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (lastName !== "") {
+            addPerson(firstName, lastName, dob);
+        }
+    }
+
+    const addPerson = (newFirst, newLast, newDOB) => {
+        let newPerson = {
+            id: crowd.length + 1,
+            firstName: newFirst,
+            lastName: newLast,
+            dob: newDOB,
+        }
+
+        const newList = crowd.concat(newPerson);
+
+        const sorted = newList.sort((a, b) => {
+            if (a.lastName < b.lastName) {
+                return -1;
+            } else if (a.lastName > b.lastName) {
+                return 1;
+            }
+            return 0;
+        })
+
+        setCrowd(sorted);
+        setFirstName("");
+        setLastName("");
+        setDob("");
+    }
+
     return (
         <Fragment>
             <hr />
@@ -58,7 +91,7 @@ function App(props) {
             <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>Toggle isTrue</a>
 
             <hr/>
-                <form autoComplete="off">
+                <form autoComplete="off" onSubmit={handleSubmit}>
                     <div className={"mb-3"}>
                         <label className={"form-label"} htmlFor={"first-name"}>First Name</label>
                         <input
@@ -88,6 +121,9 @@ function App(props) {
                         className={"form-control"}
                         onChange={(event) => setDob(event.target.value)}
                     ></Input>
+
+
+                    <input type={"submit"} value={"Submit"} className={"btn btn-primary"}></input>
 
                 </form>
 
